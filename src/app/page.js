a364a4c4f0acf5c7061c3a89132deb482ff50884
WaseemAzam901 <getwaseemazam@gmail.com>
2024-07-16 17:44:22 +0400

    Middle Section Done

--- src/app/page.js
+++ src/app/page.js
@@ -70,31 +70,141 @@ export default function Home() {
 
         <div className="flex items-center h-10 text-white bg-[#222F3D] pl-4">
           <div className="flex items-center gap-1 border border-transparent hover:border-white p-2">
-            <GiHamburgerMenu className="text-xl"/>
+            <GiHamburgerMenu className="text-xl" />
             <p className="font-bold">All</p>
           </div>
 
           <ul className="flex items-center">
             <li>
-              <select name="" id="" className="bg-transparent border border-transparent hover:border-white p-2">
+              <select
+                name=""
+                id=""
+                className="bg-transparent border border-transparent hover:border-white p-2"
+              >
                 <option value="">Fresh</option>
               </select>
             </li>
-          
-          <li className="border border-transparent hover:border-white p-2">Best Sellers</li>
-          <li className="border border-transparent hover:border-white p-2">New Releases</li>
-          <li className="border border-transparent hover:border-white p-2">Today's Deals</li>
-          <li>
-            <select name="" id="" className="bg-transparent border border-transparent hover:border-white p-2">
-              <option value="">Prime</option>
-            </select>
-          </li>
-          <li className="border border-transparent hover:border-white p-2">Elextronics</li>
-          <li className="border border-transparent hover:border-white p-2">Mobile Phones</li>
+
+            <li className="border border-transparent hover:border-white p-2">
+              Best Sellers
+            </li>
+            <li className="border border-transparent hover:border-white p-2">
+              New Releases
+            </li>
+            <li className="border border-transparent hover:border-white p-2">
+              Today's Deals
+            </li>
+            <li>
+              <select
+                name=""
+                id=""
+                className="bg-transparent border border-transparent hover:border-white p-2"
+              >
+                <option value="">Prime</option>
+              </select>
+            </li>
+            <li className="border border-transparent hover:border-white p-2">
+              Elextronics
+            </li>
+            <li className="border border-transparent hover:border-white p-2">
+              Mobile Phones
+            </li>
           </ul>
         </div>
       </header>
-      <main></main>
+
+      <main className="bg-[#E6E6E6]">
+        <div className="bg-[url('/assests/banner.jpg')] h-80 bg-cover flex items-end justify-center text-sm">
+          <p className="bg-white h-10 flex items-center justify-center w-[1460px] mb-6">
+            You are on amazon.com. You can also shop on Amazon India for
+            millions of products with fast local delivery.&nbsp;
+            <a className="text-[#007185] hover:underline hover:text-orange-500">
+              Click here to go to amazon.in{" "}
+            </a>
+          </p>
+        </div>
+
+
+
+        <div className="grid grid-cols-4 grid-rows-2 gap-4 px-4">
+
+          {/* bus checking k liye kiya tha k check karu k hum grid say kesay 2 rows aur 2 columns bana saktay hain */}
+          <div className="bg-white p-4">
+            <p className="text-xl font-bold">Shop School Essenstials</p>
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 px-4 py-4">
+            <div>
+              <a href=""><img src="/assests/1.jpg" alt="" /></a>
+            </div>
+            <div>
+            <a href=""><img src="/assests/5.jpg" alt="" /></a>
+            </div>
+            <div>
+            <a href=""><img src="/assests/6.jpg" alt="" /></a>
+            </div>
+            <div>
+            <a href=""><img src="/assests/7.jpg" alt="" /></a>
+            </div>
+            </div>
+            <a href="" className="text-[#007185] text-xs hover:text-orange-500">
+              See more
+            </a>
+          </div>
+
+
+          <div className="bg-white p-4">
+            <p className="text-xl font-bold">Home & Kitchen Under $30</p>
+            <img src="/assests/2.jpg" alt="" className="my-3" />
+            <a href="" className="text-xs text-[#007185] hover:text-orange-500">
+              Shop now
+            </a>
+          </div>
+
+
+          <div className="bg-white p-4">
+            <p className="font-bold text-xl">Health & Personal Care</p>
+            <img src="/assests/3.jpg" alt="" className="my-3" />
+            <a href="" className="text-xs hover:text-orange-500 text-[#007185]">
+              Shop now
+            </a>
+          </div>
+
+
+          <div className="bg-white p-4">
+            <p className="text-xl font-bold">Beauty Picks</p>
+            <img src="/assests/4.jpg" alt="" className="my-3"/>
+            <a href="" className="text-xs hover:text-orange-500 text-[#007185]">Shop now</a>
+          </div>
+
+
+          <div className="bg-white p-4">
+            <p className="text-xl font-bold">Electronics</p>
+            <img src="/assests/5.jpg" alt="" className="my-3"/>
+            <a href="" className="text-xs hover:text-orange-500 text-[#007185]">Shop now</a>
+          </div>
+
+
+          <div className="bg-white p-4">
+            <p className="text-xl font-bold">Toys under $30</p>
+            <img src="/assests/6.jpg" alt="" className="my-3"/>
+            <a href="" className="text-xs hover:text-orange-500 text-[#007185]">Shop now</a>
+          </div>
+
+
+          <div className="bg-white p-4">
+            <p className="text-xl font-bold">For your Fitness Needs</p>
+            <img src="/assests/7.jpg" alt="" className="my-3"/>
+            <a href="" className="text-xs hover:text-orange-500 text-[#007185]">Shop now</a>
+          </div>
+
+
+          <div className="bg-white p-4">
+            <p className="font-bold text-xl">Discover Fashion Trends</p>
+            <img src="/assests/8.jpg" alt="" className="my-3"/>
+            <a href="" className="text-xs hover:text-orange-500 text-[#007185]">Shop now</a>
+          </div>
+
+        </div>
+      </main>
       <footer></footer>
     </>
   );
